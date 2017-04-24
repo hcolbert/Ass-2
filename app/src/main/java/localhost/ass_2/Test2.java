c6b36baf8336dbb7ed0e45c3749e188bf823ad4b
hcolbert <hcolbert@utas.edu.au>
2017-04-24 13:35:47 +1000

    test from hugh to repo with comment

--- app/src/main/java/localhost/ass_2/Test2.java
+++ app/src/main/java/localhost/ass_2/Test2.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Test2 extends AppCompatActivity {
+    // still testing.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
