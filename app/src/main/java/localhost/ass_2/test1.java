67e0545e7609a31a5c36b40ec927d73921ad35c8
Dominic Weller <djweller@utas.edu.au>
2017-04-24 13:39:27 +1000

    added comment

--- app/src/main/java/localhost/ass_2/test1.java
+++ app/src/main/java/localhost/ass_2/test1.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class test1 extends AppCompatActivity {
-
+//hello this is a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
